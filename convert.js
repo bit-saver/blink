c180a8f823b47bdf6ef37ed66bae3fc3fbd7bdf7
Tyler Hebenstreit <tyler.hebenstreit@breaktech.com>
2024-09-23 13:55:59 -0500

    Updated conversion colors to hex

--- convert.js
+++ convert.js
@@ -1,14 +1,34 @@
 import fs from 'fs';
 import plist from 'plist';
 
-const files = fs.readdirSync('./').filter((f) => f.endsWith('.itermcolors'));
+function componentToHex(c) {
+  const hex = c.toString(16)
+  return hex.length === 1 ? `0${hex}` : hex
+}
 
-console.log(files);
+const files = fs.readdirSync('./').filter((f) => f.endsWith('.itermcolors'));
 
 async function convert(file) {
   const raw = fs.readFileSync(file, 'utf8');
   const xml = await plist.parse(raw);
   /**
+   *   {{ Ansi_0_Color }} // black
+   *   {{ Ansi_1_Color }} // red
+   *   {{ Ansi_2_Color }} // green
+   *   {{ Ansi_3_Color }} // yellow
+   *   {{ Ansi_4_Color }} // blue
+   *   {{ Ansi_5_Color }} // magenta
+   *   {{ Ansi_6_Color }} // cyan
+   *   {{ Ansi_7_Color }} // white
+   *   {{ Ansi_8_Color }} // bright black
+   *   {{ Ansi_9_Color }} // bright red
+   *   {{ Ansi_10_Color }} // bright green
+   *   {{ Ansi_11_Color }} // bright yellow
+   *   {{ Ansi_12_Color }} // bright blue
+   *   {{ Ansi_13_Color }} // bright magenta
+   *   {{ Ansi_14_Color }} // bright cyan
+   *   {{ Ansi_15_Color }} // bright white
+   *
    * t.prefs_.set('color-palette-overrides',
    *                  [ black , red     , green  , yellow,
    *                   blue     , magenta , cyan   , white,
@@ -19,7 +39,8 @@ async function convert(file) {
    * t.prefs_.set('foreground-color', '#000000');
    * t.prefs_.set('background-color', white);
    */
-  const rgb = (data) => `rgba(${data['Red Component']*255},${data['Green Component']*255},${data['Blue Component']*255},${data['Alpha Component']})`;
+  const comp = (data, color) => componentToHex(Math.round(data[`${color} Component`] * 255))
+  const rgb = (data) => `#${comp(data, 'Red')}${comp(data, 'Green')}${comp(data, 'Blue')}`;
   const colors = [];
   for (let i = 1; i <= 15; i++) {
     colors.push(rgb(xml[`Ansi ${i} Color`]));
@@ -29,7 +50,9 @@ async function convert(file) {
   lines.push(`t.prefs_.set('cursor-color', '${rgb(xml['Cursor Color'])}');`);
   lines.push(`t.prefs_.set('foreground-color', '${rgb(xml['Foreground Color'])}');`);
   lines.push(`t.prefs_.set('background-color', '${rgb(xml['Background Color'])}');`);
-  fs.writeFileSync(file.replace('.itermcolors', '.js'), lines.join('\n'));
+  const jsFile = file.replace('.itermcolors', '.js');
+  fs.writeFileSync(jsFile, lines.join('\n'));
+  console.log(`Converted: ${jsFile}`);
 }
 
 files.map(convert);
